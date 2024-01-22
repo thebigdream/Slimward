cbcf41a68db7513adefbb09fe0ae41a5631e0f0d
thebigdream <cliffjamau@gmail.com>
2024-01-22 12:27:08 +1000

    Minor tweaks

--- index.mjs
+++ index.mjs
@@ -29,39 +29,41 @@ client.on("ready", async () => {
 })
 
 // Send a random message occasionally
-setInterval(async() => { if (random.int(0,100) == 0) await channel.send(await novelAPI.generateText(novelAPI.preset, `Enward: How's the weather today, raining cats and dogs?\nEnward: Yesterday I saw a snail and it looked at me funny. I stared back and pulled my tongue out.\nEnward: Do you think Antarctica and Switzerland are really going to war? The thought keeps me up at night.\nEnward: I don't understand jokes about deez nuts. What's so funny about nuts? Oh wait. Testicles.\nEnward: So what's the deal with airplane food? Rhetorical question.\nEnward:`, 1, 64))}, 6000)
+setInterval(async() => { if (random.int(0,100) == 0) {
+    try { await channel.send(await novelAPI.generateText(novelAPI.preset, `Enward: How's the weather today, raining cats and dogs?\nEnward: Yesterday I saw a snail and it looked at me funny. I stared back and pulled my tongue out.\nEnward: Get in bitch we're going shopping.\nEnward: Do you think Antarctica and Switzerland are really going to war? The thought keeps me up at night.\nEnward: I don't understand jokes about deez nuts. What's so funny about nuts? Oh wait. Testicles.\nEnward: So what's the deal with airplane food? Rhetorical question.\nEnward:`, 1, 64)) } catch { console.error('Error:', error) }}
+}, 6000)
 
 // Listen for messages and reply if valid
 client.on("messageCreate", async (message) => {
-    if (!message) return
+    try {
+        // Save message locally
+        messages.push({"id":message.id, "parent":message.reference, "time":message.createdTimestamp, "author":func.getName(message), "content":func.sanitise(message.content)}) //push latest message to messages array
 
-    // Save message locally
-    messages.push({"id":message.id, "parent":message.reference, "time":message.createdTimestamp, "author":func.getName(message), "content":func.sanitise(message.content)}) //push latest message to messages array
+        // Check that channel is appropriate and author isn't Enward
+        if ((message.channel.id == channel) && (message.author.id != 1068439682460942407) && (message.content.includes('1068439682460942407') || message.mentions.has(client.user) || message.content.toLowerCase().includes('enward'))) { 
+            
+            // Build chat history using previous messages (if available)
+            var query = messages[messages.length-1] // Start query at latest message
+            var prompt = []
+            var exitLoop = false
 
-    // Check that channel is appropriate and author isn't Enward
-    if ((message.channel.id == channel) && (message.author.id != 1068439682460942407) && (message.content.includes('1068439682460942407') || message.mentions.has(client.user) || message.content.toLowerCase().includes('enward'))) { 
-        
-        // Build chat history using previous messages (if available)
-        var query = messages[messages.length-1] // Start query at latest message
-        var prompt = []
-        var exitLoop = false
+            // Log previous messages so long as query does not fail to find another message, and exitloop flag hasn't been triggered
+            while (typeof query !== 'undefined' && !exitLoop) { 
+                try {                   
+                    prompt.unshift(func.sanitise(query.author) + ': ' + func.sanitise(query.content))
+                    query = messages.find(function(messages) { return messages.id === query.parent.messageId })
+                } catch { query = undefined }
+            }
 
-        // Log previous messages so long as query does not fail to find another message, and exitloop flag hasn't been triggered
-        while (typeof query !== 'undefined' && !exitLoop) { 
-            try {                   
-                prompt.unshift(func.sanitise(query.author) + ': ' + func.sanitise(query.content))
-                query = messages.find(function(messages) { return messages.id === query.parent.messageId })
-            } catch { query = undefined }
-        }
+            // Prepare response
+            prompt = `[This is a Discord server known as Mafia Server.]\n----\n[Enward is a witty, whimsical, enigmatic guy who gives long responses to others. He has a yellow face with beckoning eyes and a wicked smile. He is friends with Gug and JOM.]\n----\n[Style: chat, chatroom.]\n${prompt.join("\n")}\nEnward:` // Add personality and style, then prime Enward's response
+            if (prompt.length > 8000) prompt = prompt.substring(prompt.length - 8000) // Ensure prompt is less than ~4000 tokens
+            var response = await novelAPI.generateText(novelAPI.preset, prompt, 1, 64)
+            console.log(prompt)
 
-        // Prepare response
-        prompt = `[This is a Discord server known as Mafia Server.]\n----\n[Enward is a witty, whimsical, enigmatic guy who gives long responses to others. He has a yellow face with beckoning eyes and a wicked smile. He is friends with Gug and JOM.]\n----\n[Style: chat, chatroom.]\n${prompt.join("\n")}\nEnward:` // Add personality and style, then prime Enward's response
-        if (prompt.length > 8000) prompt = prompt.substring(prompt.length - 8000) // Ensure prompt is less than ~4000 tokens
-        var response = await novelAPI.generateText(novelAPI.preset, prompt, 1, 64)
-        console.log(prompt)
-
-        // Reply
-        func.reply(message, func.sanitise(response))
-        console.log(response)
-    }
+            // Reply
+            func.reply(message, func.sanitise(response))
+            console.log(response)
+        } 
+    } catch { console.error('Error:', error) }
 })
